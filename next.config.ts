5a5cd1feeb9968b1f9290eec1452d31c4313c2ea
mstagon <devcms21@gmail.com>
2025-04-21 18:43:07 +0900

    feat: next.config.ts 수정 오류 수정
    Related to: #1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "standalone", // standalone 모드 활성화
-    experimental: {
-        appDir: true, // App Router 활성화
-    },
+    output: "standalone",
 };
 
 export default nextConfig;
