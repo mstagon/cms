67a437eb278613e34bac6237d091ba20aeef1fb0
mstagon <devcms21@gmail.com>
2025-04-22 01:54:44 +0900

    feat: 인트로 페이지 배경 영상 수정
    화면 크기에 맞게 꽉차게 변경
    
    Related to: #3

--- app/components/VideoComponents.tsx
+++ app/components/VideoComponents.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 const VideoComponent = () => {
-  return (
-    <video
-      autoPlay
-      playsInline
-      loop
-      muted
-      width={"100%"}
-      height={"100%"}
-      preload="none"
-      src="/background1.mp4"
-      style={{
-        objectFit: "cover",
-      }}
-    />
-  );
+    return (
+        <video
+            autoPlay
+            playsInline
+            loop
+            muted
+            preload="none"
+            src="/background1.mp4"
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+            }}
+        />
+    );
 };
 
 export default VideoComponent;
